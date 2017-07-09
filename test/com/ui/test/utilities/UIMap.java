4da75c25ce737becdf4e049373a2da07f5d2a3a1
Your Name <todakar.sagar@gmail.com>
2017-07-10 01:07:36 +0530

    intial commit to develop_sagar

--- test/com/ui/test/utilities/UIMap.java
+++ test/com/ui/test/utilities/UIMap.java
@@ -4,18 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByXPath;
 
 public class UIMap {
-	
-	//------------------Put all the Xpaths in this section--------------------------------
-	
-	//------------------Put all the Xpaths in this section--------------------------------	
-	
-	//------------------Put all the Locators in this section--------------------------------	
+		
+	//------------------all the Locators in this section--------------------------------	
 	
 	public static By loginEditBoxName=By.name("username");
 	public static By loginPagePasswrdEditBoxname=By.name("password");
 	public static By loginPageSignInButtonName=By.className("button");
 
 	
-	//------------------Put all the Locators in this section--------------------------------	
+
 
 }
